eb97f1cb3d8520199ad79e66a0cb156e078d157e
LENOVOLEGIONEA\Azby <ewenn.ansq@gmail.com>
2025-06-14 22:17:42 +0200

    Ajout d'un backend Flask avec intégration avec docker-compose.

--- SatisfactoryMapFrontEnd/src/App.tsx
+++ SatisfactoryMapFrontEnd/src/App.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
 import './App.css'
 
 function App() {
+  const [array, setArray] = useState([]);
+
+  const fetchAPI = async () =>{
+    const response = await axios.get("http://localhost:8080/api/users")
+    setArray(response.data.users)
+  };
+
+  useEffect(()=>{
+    fetchAPI()
+  },[])
 
   return (
     <div>
       <h1>Satisfactory Map</h1>
+        {array.map((user, index) => (
+          <div key={index}>
+            <span>{user}</span><br></br>
+          </div>
+        ))}
     </div>
   )
 }
