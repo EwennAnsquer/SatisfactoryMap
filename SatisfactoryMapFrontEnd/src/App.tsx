fd1101f6a136d3e00bc9b0ca493dcc059735bde7
LENOVOLEGIONEA\Azby <ewenn.ansq@gmail.com>
2025-06-17 22:23:38 +0200

    Ajout d'une connexion obligatoire pour uttilisé le site grâce à keycloak.

--- SatisfactoryMapFrontEnd/src/App.tsx
+++ SatisfactoryMapFrontEnd/src/App.tsx
@@ -1,7 +1,41 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
+import Keycloak from 'keycloak-js'
 
 import './App.css'
+
+let initOptions = {
+  url: 'http://localhost:4000/',
+  realm: 'SatisfactoryMapRealm',
+  clientId: 'SatisfactoryMapClient',
+}
+
+let kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: 'login-required',
+  checkLoginIframe: false,
+  pkceMethod: 'S256'
+}).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    console.info("Authenticated");
+    console.log('auth', auth)
+    console.log('Keycloak', kc)
+    console.log('Access Token', kc.token)
+
+    /* http client will use this header in every request it sends */
+    axios.defaults.headers.common['Authorization'] = `Bearer ${kc.token}`;
+
+    kc.onTokenExpired = () => {
+      console.log('token expired')
+    }
+  }
+}, () => {
+  /* Notify the user if necessary */
+  console.error("Authentication Failed");
+});
 
 function App() {
   const [array, setArray] = useState([]);
@@ -29,6 +63,27 @@ function App() {
           </div>
         ))}
         <img src={`${minioURL}/image.webp`} alt="My Image" />
+        <button onClick={() => { alert(kc.authenticated ? 'Authenticated: TRUE' : 'Authenticated: FALSE') }}>
+          Is Authenticated
+        </button>
+        <button onClick={() => { kc.login() }}>
+          login
+        </button>
+        <button onClick={() => { alert(kc.token) }}>
+          Show Access Token
+        </button>
+        <button onClick={() => { alert(JSON.stringify(kc.tokenParsed)) }}>
+          Show Parsed Access Token
+        </button>
+        <button onClick={() => { alert(kc.isTokenExpired(5).toString()) }}>
+          Check Token Expire
+        </button>
+        <button onClick={() => { kc.updateToken(10).then((refreshed) => { alert('Token Refreshed: ' + refreshed.toString()) }, (e) => { alert('Refresh Error') }) }}>
+          UPDATE Token (if about to expire)
+        </button> 
+        <button onClick={() => { kc.logout({ redirectUri: 'http://localhost:5173/' }) }}>
+          logout
+        </button>
     </div>
   )
 }
